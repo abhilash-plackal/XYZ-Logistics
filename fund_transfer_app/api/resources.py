e8959944d9af37a0ad3cc8c42550c1bb751ec0c1
abhips <abhilash@plackal.in>
2017-12-04 02:31:32 +0530

    update logic for fund transfer

--- fund_transfer_app/api/resources.py
+++ fund_transfer_app/api/resources.py
@@ -47,46 +47,58 @@ class TransactionResource(ModelResource):
         user = bundle.request.user if bundle.request else None
 
         if (Group.objects.get(name='root_users').user_set.filter(id=user.id).exists()):
-            from_list = Node.objects.filter(name__iexact=bundle.data['from'])
-            to_list = Node.objects.filter(name__iexact=bundle.data['to'])
+            from_list = Node.objects.filter(name__exact=bundle.data['from'])
+            to_list = Node.objects.filter(name__exact=bundle.data['to'])
+
+            to_obj = None
+            from_obj = None
+            amount = Decimal(bundle.data['amount'])
 
             if from_list.count() != 0:
                 from_obj = from_list[0]
 
             if to_list.count() != 0:
                 to_obj = to_list[0]
-            
-            amount = Decimal(bundle.data['amount'])
-            if (to_obj.name == from_obj.parent.name) or (from_obj.name == to_obj.parent.name):
-                if from_obj.balance < amount:
-                    print('Insufficient fund!!')
-                else:
-                    to_obj.balance = to_obj.balance + amount
-                    to_obj.save()
 
-                    from_obj.balance = from_obj.balance - amount
-                    from_obj.save()
+            if from_obj.balance < amount:
+                print('Insufficient fund!!')
+                return bundle
 
-                    bundle.data['amount'] = amount
-                    bundle.data['node'] = from_obj
+            if (from_obj == None) or (to_obj == None):
+                return bundle 
 
-                    str_date = str(datetime.datetime.now())
-                    bundle.data['transaction_date'] = str_date
-                    
-                    bundle.data['actor'] = user
-                    bundle.data['transaction_type'] = 'CREDIT' if (from_obj.name == to_obj.parent.name) else 'DEBIT'
-
-                    bundle.data.pop('from', None)
-                    bundle.data.pop('to', None)
-
-                    print('-----------------------------------------------------------------------')
-                    print(bundle.data)
-                    print('-----------------------------------------------------------------------')
-
-                    return super(TransactionResource, self).obj_create(bundle, **kwargs)
+            if (from_obj.name == 'Root') and (from_obj.name == to_obj.parent.name):
+                bundle.data['node'] = from_obj
+                bundle.data['transaction_type'] = 'DEBIT'
+            elif (to_obj.name == 'Root') and (to_obj.name == from_obj.parent.name):
+                bundle.data['node'] = to_obj
+                bundle.data['transaction_type'] = 'CREDIT'
+            elif (to_obj.name == from_obj.parent.name) or (from_obj.name == to_obj.parent.name):
+                bundle.data['node'] = from_obj
+                bundle.data['transaction_type'] = 'CREDIT' if (from_obj.name == to_obj.parent.name) else 'DEBIT'
             else:
-                print('Cant be transfered from `' + from_obj.name + '` to `' + to_obj.name 
+                print('Fund cant be transfered from `' + from_obj.name + '` to `' + to_obj.name 
                                             + "` since they don't have a parent child relationship")
+                return bundle
+
+
+            to_obj.balance = to_obj.balance + amount
+            to_obj.save()
+
+            from_obj.balance = from_obj.balance - amount
+            from_obj.save()
+
+            bundle.data['amount'] = amount
+
+            str_date = str(datetime.datetime.now())
+            bundle.data['transaction_date'] = str_date
+            
+            bundle.data['actor'] = user
+
+            bundle.data.pop('from', None)
+            bundle.data.pop('to', None)
+
+            return super(TransactionResource, self).obj_create(bundle, **kwargs)   
 
         return bundle 
 
